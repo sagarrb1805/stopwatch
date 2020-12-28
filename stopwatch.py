00c43a689effd6f69fba1a3e22dfe6a0f0c24553
SagarR.B <sagarrb1805@gmail.com>
2020-12-28 15:03:36 +0530

    added the buttons

--- stopwatch.py
+++ stopwatch.py
@@ -20,10 +20,28 @@ today_date = date.today()
 date_label = Label(root, text=today_date, relief="solid", width=20, font=("arial", 20, "bold"), bg="grey")
 date_label.place(x=76, y=220)
 
-watch_time = "{} : {} : {}".format(00, 00, 00)
 
+
+
+watch_time = "{} : {} : {}".format(00, 00, 00)
 watch_label = Label(root, text=watch_time, relief="solid", width=20, bg="grey", font=("arial", 20, "bold"))
 watch_label.place(x=76, y=280)
+
+def start():
+    print("start")
+def stop():
+    print("stop")
+def restart():
+    print("restart")
+
+start_btn = Button(root, text="Start", width=12, bg="grey", fg="white", command=start)
+start_btn.place(x=76, y=350)
+
+stop_btn = Button(root, text="Stop", width=12, bg="grey", fg="white", command=stop)
+stop_btn.place(x=328, y=350)
+
+restart_btn = Button(root, text="Restart", width=12, bg="grey", fg="white", command=restart)
+restart_btn.place(x=203, y=350)
 
 
 root.mainloop()
